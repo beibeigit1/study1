b36359a75ddcf6f5874d19c1eaf525a5f0218288
beibeigit1 <1239956958@qq.com>
2019-04-08 21:38:33 +0800

    firstapp

---
+++ firstvue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <c-header></c-header>
+    <c-footer></c-footer>
+  </div>
+</template>
+<script>
+import CHeader from '@/components/CHeader';//引入公共子组件
+import CFooter from '@/components/CFooter';
+
+export default {
+  components:{
+    CHeader,
+    CFooter
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ firstvue/src/components/CHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="header">首页</div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
